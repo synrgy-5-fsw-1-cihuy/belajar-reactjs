e6cd7ce79c5ac8cf114bdacb10710a221b52b4fd
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-12-07 20:28:30 +0700

    styled comp

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,10 +1,20 @@
 import style from "./Content.module.css";
+import {
+    Div, H1
+} from "./StyledDiv.js";
 
 const Content = (props) => {
     return(
         <div>
+            {/* CSS Module */}
             <p className={style["Content"]}>Content component is works!</p>
             {props.name} - {props.age}
+            <Div>
+                Halo
+                <H1>
+                    Judul coba dulu
+                </H1>
+            </Div>
 
         </div>
     );
