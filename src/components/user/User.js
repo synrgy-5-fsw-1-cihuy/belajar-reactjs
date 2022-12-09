cccbe030c5c6ae77b52811d653b8577e1440689b
Reja <rezanurrochmat@gmail.com>
2022-12-09 20:48:13 +0700

    post request

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const USER_ENDPOINT_URL = 'https://jsonplaceholder.typicode.com/users';
+const POST_ENDPOINT_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 const User = () => {
     const [users, setUsers] = useState([]);
@@ -13,12 +14,25 @@ const User = () => {
         setUsers(users.data);
     };
 
+    async function createNewPost() {
+        const payload = {
+            title: 'Joo',
+            body: 'Ganteng',
+            userId: 1,
+        };
+
+        const createdPost = await axios.post(POST_ENDPOINT_URL, payload);
+
+        console.log(createdPost.data);
+    }
+
     useEffect(() => {
         getAllUsers();
     }, []);
 
     return (
         <div>
+            <button className="btn btn-primary" onClick={() => createNewPost()}>Add Posts</button>
             <table>
                 <thead>
                     <tr>
