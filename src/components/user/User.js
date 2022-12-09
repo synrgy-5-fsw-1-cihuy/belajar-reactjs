5c28c546a633386583653374761e1f03014545f6
Reja <rezanurrochmat@gmail.com>
2022-12-09 21:26:20 +0700

    add upload with files

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -3,9 +3,11 @@ import { useEffect, useState } from "react";
 
 const USER_ENDPOINT_URL = 'https://jsonplaceholder.typicode.com/users';
 const POST_ENDPOINT_URL = 'https://jsonplaceholder.typicode.com/posts';
+const CAR_ENDPOINT_URL = 'http://localhost:8001/api/cars';
 
 const User = () => {
     const [users, setUsers] = useState([]);
+    const [file, setFile] = useState('');
 
     async function getAllUsers() {
         const users = await axios.get(USER_ENDPOINT_URL);
@@ -24,7 +26,24 @@ const User = () => {
         const createdPost = await axios.post(POST_ENDPOINT_URL, payload);
 
         console.log(createdPost.data);
-    }
+    };
+
+    async function createNewCarWithFile() {
+        const formData = new FormData();
+        formData.append('name', 'Avanza');
+        formData.append('size', 'medium');
+        formData.append('price', '100000');
+        formData.append('foto', file);
+
+        const carCreated = await axios.post(CAR_ENDPOINT_URL, formData);
+
+        console.log(carCreated.data);
+    };
+
+    const onChangeUploadedFiles = (event) => {
+        const file = event.target.files[0];
+        setFile(file);
+    } 
 
     useEffect(() => {
         getAllUsers();
@@ -33,6 +52,8 @@ const User = () => {
     return (
         <div>
             <button className="btn btn-primary" onClick={() => createNewPost()}>Add Posts</button>
+            <input type="file" name="file" onChange={(event) => onChangeUploadedFiles(event)} />
+            <button className="btn btn-primary" onClick={() => createNewCarWithFile()}>Add Posts</button>
             <table>
                 <thead>
                     <tr>
