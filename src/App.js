84fe67f7b615925b64de8a5bf88e19c7aa0152d6
Reja <rezanurrochmat@gmail.com>
2022-12-12 20:30:54 +0700

    add oauth google

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contact from './components/contact/Contact';
 // import Content from './components/content/Content';
 import Header from './components/header/Header';
 import Login from './components/login/Login';
+import OAuth from './components/oauth/OAuth';
 import Profile from './components/profile/Profile';
 import User from './components/user/User';
 
@@ -25,6 +26,7 @@ function App() {
             <Route path='/contact' element={<Contact />} />
             <Route path='/profile' element={<Profile />} />
             <Route path='/user' element={<User />} />
+            <Route path='/' element={<OAuth />} />
          </Routes>
       </Router>
       {/* <Content 
---
+++ src/components/oauth/OAuth.js
@@ -0,0 +1,20 @@
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+
+const OAuth = () => {
+    return(
+        <div>
+           <GoogleOAuthProvider clientId='337583218677-lc28k1240qlatd8099jv3cujb86j9uis.apps.googleusercontent.com'>
+           <GoogleLogin
+            onSuccess={credentialResponse => {
+                console.log(credentialResponse);
+            }}
+            onError={() => {
+                console.log('Login Failed');
+            }}
+            />;
+           </GoogleOAuthProvider>
+        </div>
+    )
+};
+
+export default OAuth;
