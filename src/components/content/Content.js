97fa2e5e7c543fd87a1b5d8262e2e7ad3e9d5518
Reja <rezanurrochmat@gmail.com>
2022-12-15 22:03:24 +0700

    add filter capacity refactor

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -10,6 +10,11 @@ const CARS_ENDPOINT_URL = 'http://localhost:8001/api/cars';
 
 const Content = () => {
     const [cars, setCars] = useState([]);
+    const [filterCarParams, setFilterCar] = useState({
+        capacity: '',
+        booking_date: '',
+        booking_time: ''
+    });
 
     useEffect(() => {
         getAllCars();
@@ -20,27 +25,74 @@ const Content = () => {
         setCars(cars.data.data);
     };
 
-    const onFilterCars = async (event) => {
-        const capacity = event.target.value;
+    const onFilterCarCapacityParam = async (event) => {
+        setFilterCar((prevState) => ({
+            ...prevState,
+            capacity: event.target.value
+        }));
+    };
 
-        if (capacity === "") {
-            getAllCars();
-        } else {
-            const filterCars = await axios.get(CARS_ENDPOINT_URL + '?capacity=' + capacity);
-            setCars(filterCars.data.data);
-        }
-        
+    const onFilterCarBookingDateParam = async(event) => {
+        setFilterCar((prevState) => ({
+            ...prevState,
+            booking_date: event.target.value
+        }));
+    };
+
+    const onFilterCarBookingTimeParam = async(event) => {
+        setFilterCar((prevState) => ({
+            ...prevState,
+            booking_time: event.target.value
+        }));
+    };
+
+    const onEventSubmitFilter = (event) => {
+        const bookingDatesTime = filterCarParams.booking_date +'T'+ filterCarParams.booking_time + ':00Z';
+        const bookingDateParsed = Date.parse(bookingDatesTime);
+
+        onShowFilterCars(filterCarParams);
+
+        event.preventDefault();
+    };
+
+    const onShowFilterCars = async (filter) => {
+        if (filter.capacity != null) {
+            const cars = await axios.get(CARS_ENDPOINT_URL + '?capacity=' + filter.capacity);
+            setCars(cars.data.data);
+        };
     };
 
     return (
         <div>
             <Container>
-                <label>Filter by capacity</label>
-                <input type="number" onChange={(event) => onFilterCars(event)} />
+                <form onSubmit={(event) => onEventSubmitFilter(event)}>
+                    <div>
+                        <label>Filter by capacity</label>
+                        <input type="number" onChange={(event) => onFilterCarCapacityParam(event)} />
+                    </div>
+                    <div>
+                        <label>Filter by Date</label>
+                        <input type="date" onChange={(event) => onFilterCarBookingDateParam(event)} />
+                    </div>
+                    <div>
+                        <label>Filter by Time</label>
+                        <select onChange={(event) => onFilterCarBookingTimeParam(event)}>
+                            <option>Select waktu</option>
+                            <option value="08:00">08.00</option>
+                            <option value="09:00">09.00</option>
+                            <option value="10:00">10.00</option>
+                            <option value="11:00">11.00</option>
+                            <option value="12:00">12.00</option>
+                        </select>
+                    </div>
+                    <div>
+                        <button type='submit'>Submit</button>
+                    </div>
+                </form>
                 <Row>
-                    { cars.map((car) => {
+                    { cars.map((car, index) => {
                         return <Col>
-                        <Card style={{ width: '18rem' }}>
+                        <Card key={index} style={{ width: '18rem' }}>
                             <Card.Img variant="top" src={car.foto} />
                             <Card.Body>
                                 <Card.Title>{car.name}</Card.Title>
