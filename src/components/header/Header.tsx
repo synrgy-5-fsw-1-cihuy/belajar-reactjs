b04a701286c25a64ff2f699a36fe5baef9a3f3d0
Reja <rezanurrochmat@gmail.com>
2022-12-26 20:03:15 +0700

    fix error header comp

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,16 @@
+import React from 'react';
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { addTodo, removeTodo } from '../store/Actions';
 
+type HeaderProps = {
+  todoState: any;
+  addNewTodo: any;
+  removeTodoRecord: any;
+};
+
 // Functional component
-const Header = ({ todoState, addNewTodo, removeTodoRecord }) => {
+const Header = ({ todoState, addNewTodo, removeTodoRecord }: HeaderProps) => {
   // Effect Hook
   useEffect(() => {
     console.log(todoState);
@@ -18,23 +25,23 @@ const Header = ({ todoState, addNewTodo, removeTodoRecord }) => {
   };
 
   return (
-    <div>
+    <div className="Header">
       Header components is works!
       <button onClick={() => handleAddNewTodo()}>Add New Todo</button>
       <button onClick={() => handleRemoveTodo()}>Remove Todo</button>
-      {todoState.todos.map((todo, index) => {
+      {todoState.todos.map((todo: any, index: any) => {
         return <li key={index}>{todo}</li>;
       })}
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state: any) => ({
   todoState: state.todoReducer
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  addNewTodo: (todo) => dispatch(addTodo(todo)),
+const mapDispatchToProps = (dispatch: any) => ({
+  addNewTodo: (todo: any) => dispatch(addTodo(todo)),
   removeTodoRecord: () => dispatch(removeTodo())
 });
 
